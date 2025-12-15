5c0236a1cb24a83aeb1b0fe25e8f62b90092e3a3
Emcikem <48344553+Emcikem@users.noreply.github.com>
2025-12-15 14:55:06 +0000

    Deploying to static-pages from @ llm-manus/llmops-vue@494b869e25857091f17a7596bd9950453dffb2f4 🚀

---
+++ assets/oauth-B__VFUjv.js
@@ -0,0 +1,1 @@
+import{p as r,g as a}from"./index-Cb8ybpZu.js";const s=o=>a(`/oauth/${o}`),p=(o,t)=>r(`/oauth/authorize/${o}`,{body:{code:t}});export{p as a,s as p};
--- assets/oauth-DPuAKwDO.js
+++
@@ -1,1 +0,0 @@
-import{p as r,g as a}from"./index-Dwc_z1ot.js";const s=o=>a(`/oauth/${o}`),p=(o,t)=>r(`/oauth/authorize/${o}`,{body:{code:t}});export{p as a,s as p};
